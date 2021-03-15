d6e0746675d85f3989181dd8a056d7e0531ee39a
Oussama Filani <42185573+filani07@users.noreply.github.com>
2021-03-15 23:14:20 +0100

    database

--- database/db_apprenants.sql
+++ database/db_apprenants.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 12, 2021 at 02:06 AM
+-- Generation Time: Mar 15, 2021 at 10:03 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.2.34
 
@@ -29,11 +29,23 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `absence` (
   `id_absence` int(11) NOT NULL,
-  `id_seance` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
   `justifiee` tinyint(1) NOT NULL,
-  `comm_abs` text COLLATE utf8_bin DEFAULT NULL
+  `comm_abs` text COLLATE utf8_bin DEFAULT NULL,
+  `fk_seance` int(11) NOT NULL,
+  `fk_etudiant` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `absence`
+--
+
+INSERT INTO `absence` (`id_absence`, `justifiee`, `comm_abs`, `fk_seance`, `fk_etudiant`) VALUES
+(1, 1, 'malade', 1, 4),
+(2, 0, NULL, 1, 6),
+(3, 1, NULL, 5, 6),
+(4, 0, NULL, 6, 4),
+(5, 1, 'nonnnn', 12, 1),
+(6, 1, 'OUII', 7, 8);
 
 -- --------------------------------------------------------
 
@@ -42,20 +54,29 @@ CREATE TABLE `absence` (
 --
 
 CREATE TABLE `enseignant` (
-  `id_user` int(11) NOT NULL,
+  `id_enseignant` int(11) NOT NULL,
   `nom_ens` varchar(25) COLLATE utf8_bin NOT NULL,
   `prenom_ens` varchar(25) COLLATE utf8_bin NOT NULL,
-  `email_ens` varchar(25) COLLATE utf8_bin NOT NULL
+  `email_ens` varchar(25) COLLATE utf8_bin NOT NULL,
+  `fk_user` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `enseignant`
+--
+
+INSERT INTO `enseignant` (`id_enseignant`, `nom_ens`, `prenom_ens`, `email_ens`, `fk_user`) VALUES
+(1, 'oussama', 'oussama', 'oussama@gmail.com', 1),
+(2, 'anas', 'anas', 'anas@gmail.com', 2);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table  `etudiant`
+-- Table structure for table `etudiant`
 --
 
 CREATE TABLE `etudiant` (
-  `id_user` int(11) NOT NULL,
+  `id_etudiant` int(11) NOT NULL,
   `CNE` int(11) NOT NULL,
   `nom_etu` varchar(25) COLLATE utf8_bin NOT NULL,
   `prenom_etu` varchar(25) COLLATE utf8_bin NOT NULL,
@@ -63,8 +84,23 @@ CREATE TABLE `etudiant` (
   `adresse_etu` varchar(50) COLLATE utf8_bin DEFAULT NULL,
   `ville_etu` varchar(30) COLLATE utf8_bin DEFAULT NULL,
   `email_etu` varchar(30) COLLATE utf8_bin NOT NULL,
-  `phone_etu` varchar(30) COLLATE utf8_bin DEFAULT NULL
+  `phone_etu` varchar(30) COLLATE utf8_bin DEFAULT NULL,
+  `fk_user` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `etudiant`
+--
+
+INSERT INTO `etudiant` (`id_etudiant`, `CNE`, `nom_etu`, `prenom_etu`, `date_naiss_etu`, `adresse_etu`, `ville_etu`, `email_etu`, `phone_etu`, `fk_user`) VALUES
+(1, 1155774466, 'hassan', 'Ali', '1990-12-16', 'Adresse de Alaoui Ali', 'Tanger', 'alaoui.ali@gmail.com', '0625143658', 3),
+(2, 1254879652, 'Ali', 'ilyas', '1993-04-11', 'Adresse de MORABET ilyas', 'Tetouan', 'morabet.ilyas@gmail.com', '0658472435', 4),
+(3, 88556324, 'SALHI', 'Steve', '1995-12-05', 'Adresse de Steve', 'London', 'steve.jobs@gmail.com', '0222555447', 5),
+(4, 2147483647, 'ilyas', 'Fadoua', '0000-00-00', 'Adresse de Fadoua', 'Asila', 'hanafi.fadoua@gmail.com', '0655442211', 6),
+(5, 217483247, 'farouk', 'farouk', '0000-00-00', 'Adressede Fadoua', 'Asila', 'farouk.fadoua@gmail.com', '0655442211', 7),
+(6, 27483647, 'steve', 'steve', '0000-00-00', 'Adresse de Fadoua', 'Asila', 'hanafi.fadoua@gmail.com', '0655442211', 8),
+(7, 283647, 'sara', 'sara', '0000-00-00', 'Adresse de Fadoua', 'Asila', 'sara.fadoua@gmail.com', '0655442211', 9),
+(8, 667483647, 'fadoua', 'fadoua', '0000-00-00', 'Adresse de Fadoua', 'Asila', 'fadoua.fadoua@gmail.com', '0655442211', 10);
 
 -- --------------------------------------------------------
 
@@ -74,8 +110,22 @@ CREATE TABLE `etudiant` (
 
 CREATE TABLE `module` (
   `id_module` int(11) NOT NULL,
-  `intitule_module` varchar(30) COLLATE utf8_bin NOT NULL
+  `intitule_module` varchar(30) COLLATE utf8_bin NOT NULL,
+  `fk_enseigne` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `module`
+--
+
+INSERT INTO `module` (`id_module`, `intitule_module`, `fk_enseigne`) VALUES
+(1, 'Developpement web', 1),
+(2, 'JAVA', 2),
+(3, 'Ruby', 2),
+(4, 'Linux', 1),
+(5, 'Docker', 2),
+(6, 'NoSQL', 2),
+(7, 'Javascript', 1);
 
 -- --------------------------------------------------------
 
@@ -85,10 +135,24 @@ CREATE TABLE `module` (
 
 CREATE TABLE `note` (
   `id_note` int(11) NOT NULL,
-  `id_module` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
-  `note_module` int(11) NOT NULL
+  `note_module` int(11) NOT NULL,
+  `fk_module` int(11) NOT NULL,
+  `fk_etudiant` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `note`
+--
+
+INSERT INTO `note` (`id_note`, `note_module`, `fk_module`, `fk_etudiant`) VALUES
+(1, 16, 1, 1),
+(2, 14, 1, 2),
+(3, 18, 1, 3),
+(4, 16, 1, 4),
+(5, 17, 1, 5),
+(6, 17, 1, 6),
+(7, 17, 1, 7),
+(8, 13, 1, 8);
 
 -- --------------------------------------------------------
 
@@ -98,13 +162,33 @@ CREATE TABLE `note` (
 
 CREATE TABLE `seance` (
   `id_seance` int(11) NOT NULL,
-  `id_module` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
   `date_seance` date NOT NULL,
   `heure_debut` time DEFAULT NULL,
   `heure_fin` time DEFAULT NULL,
-  `type_seance` varchar(15) COLLATE utf8_bin NOT NULL
+  `type_seance` varchar(15) COLLATE utf8_bin NOT NULL,
+  `fk_seance_module` int(11) NOT NULL,
+  `fk_seance_enseignant` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `seance`
+--
+
+INSERT INTO `seance` (`id_seance`, `date_seance`, `heure_debut`, `heure_fin`, `type_seance`, `fk_seance_module`, `fk_seance_enseignant`) VALUES
+(1, '2021-04-03', '13:30:00', '15:00:00', 'Cours', 1, 1),
+(2, '2021-04-07', '08:30:00', '10:00:00', 'TP', 1, 2),
+(3, '2021-04-11', '10:30:00', '12:00:00', 'Cours', 1, 2),
+(4, '2021-04-05', '10:30:00', '12:00:00', 'Cours', 2, 1),
+(5, '2021-04-07', '08:30:00', '10:00:00', 'TP', 2, 2),
+(6, '2021-04-06', '13:30:00', '15:00:00', 'Cours', 3, 2),
+(7, '2021-04-06', '15:30:00', '17:00:00', 'TD', 3, 1),
+(8, '2021-04-01', '15:30:00', '17:00:00', 'Cours', 4, 2),
+(9, '2021-04-02', '13:30:00', '15:00:00', 'Cours', 4, 2),
+(10, '2021-04-02', '08:30:00', '10:00:00', 'TD', 5, 2),
+(11, '2021-04-03', '10:30:00', '12:00:00', 'Cours', 5, 1),
+(12, '2021-03-01', '10:30:00', '12:00:00', 'Cours', 6, 2),
+(13, '2021-03-03', '08:30:00', '10:00:00', 'TP', 6, 1),
+(14, '2020-05-05', '08:30:00', '10:00:00', 'TP', 7, 2);
 
 -- --------------------------------------------------------
 
@@ -116,9 +200,24 @@ CREATE TABLE `user` (
   `id_user` int(11) NOT NULL,
   `login` varchar(50) COLLATE utf8_bin NOT NULL,
   `password` varchar(50) COLLATE utf8_bin NOT NULL,
-  `type_user` varchar(20) COLLATE utf8_bin NOT NULL,
   `access` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id_user`, `login`, `password`, `access`) VALUES
+(1, 'oussama', '123', 1),
+(2, 'anas', '123', 1),
+(3, 'hassan', '123', 0),
+(4, 'Ali', '123', 0),
+(5, 'SALHI', '123', 0),
+(6, 'ilyas', '123', 0),
+(7, 'farouk', '123', 0),
+(8, 'steve', '123', 0),
+(9, 'sara', '123', 0),
+(10, 'fadoua', '123', 0);
 
 --
 -- Indexes for dumped tables
@@ -129,42 +228,45 @@ CREATE TABLE `user` (
 --
 ALTER TABLE `absence`
   ADD PRIMARY KEY (`id_absence`),
-  ADD KEY `FK_Avoir` (`id_seance`),
-  ADD KEY `FK_concerne` (`id_user`);
+  ADD KEY `FK_seance` (`fk_seance`),
+  ADD KEY `FK_etudiant1` (`fk_etudiant`);
 
 --
 -- Indexes for table `enseignant`
 --
 ALTER TABLE `enseignant`
-  ADD PRIMARY KEY (`id_user`);
+  ADD PRIMARY KEY (`id_enseignant`),
+  ADD KEY `FK_ens` (`fk_user`);
 
 --
 -- Indexes for table `etudiant`
 --
 ALTER TABLE `etudiant`
-  ADD PRIMARY KEY (`id_user`);
+  ADD PRIMARY KEY (`id_etudiant`),
+  ADD KEY `FK_etu` (`fk_user`);
 
 --
 -- Indexes for table `module`
 --
 ALTER TABLE `module`
-  ADD PRIMARY KEY (`id_module`);
+  ADD PRIMARY KEY (`id_module`),
+  ADD KEY `FK_Enseigne_module` (`fk_enseigne`);
 
 --
 -- Indexes for table `note`
 --
 ALTER TABLE `note`
   ADD PRIMARY KEY (`id_note`),
-  ADD KEY `FK_Etudiant` (`id_user`),
-  ADD KEY `FK_Module` (`id_module`);
+  ADD KEY `FK_Etudiant2` (`fk_etudiant`),
+  ADD KEY `FK_Module1` (`fk_module`);
 
 --
 -- Indexes for table `seance`
 --
 ALTER TABLE `seance`
   ADD PRIMARY KEY (`id_seance`),
-  ADD KEY `FK_Enseigne` (`id_user`),
-  ADD KEY `FK_Occupe` (`id_module`);
+  ADD KEY `FK__seance_enseignant` (`fk_seance_enseignant`),
+  ADD KEY `FK__seance_module` (`fk_seance_module`);
 
 --
 -- Indexes for table `user`
@@ -180,31 +282,19 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `absence`
 --
 ALTER TABLE `absence`
-  MODIFY `id_absence` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `module`
---
-ALTER TABLE `module`
-  MODIFY `id_module` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_absence` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `note`
 --
 ALTER TABLE `note`
-  MODIFY `id_note` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `seance`
---
-ALTER TABLE `seance`
-  MODIFY `id_seance` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_note` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Constraints for dumped tables
@@ -214,34 +304,42 @@ ALTER TABLE `user`
 -- Constraints for table `absence`
 --
 ALTER TABLE `absence`
-  ADD CONSTRAINT `FK_Avoir` FOREIGN KEY (`id_seance`) REFERENCES `seance` (`id_seance`),
-  ADD CONSTRAINT `FK_concerne` FOREIGN KEY (`id_user`) REFERENCES `etudiant` (`id_user`);
+  ADD CONSTRAINT `FK_etudiant1` FOREIGN KEY (`fk_etudiant`) REFERENCES `etudiant` (`id_etudiant`),
+  ADD CONSTRAINT `FK_seance` FOREIGN KEY (`fk_seance`) REFERENCES `seance` (`id_seance`);
 
 --
 -- Constraints for table `enseignant`
 --
 ALTER TABLE `enseignant`
-  ADD CONSTRAINT `FK_Inheritance` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+  ADD CONSTRAINT `FK_ens` FOREIGN KEY (`fk_user`) REFERENCES `user` (`id_user`);
 
 --
 -- Constraints for table `etudiant`
 --
 ALTER TABLE `etudiant`
-  ADD CONSTRAINT `FK_Inheritance_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+  ADD CONSTRAINT `FK_etu` FOREIGN KEY (`fk_user`) REFERENCES `user` (`id_user`);
+
+--
+-- Constraints for table `module`
+--
+ALTER TABLE `module`
+  ADD CONSTRAINT `FK_Enseigne_module` FOREIGN KEY (`fk_enseigne`) REFERENCES `enseignant` (`id_enseignant`);
 
 --
 -- Constraints for table `note`
 --
 ALTER TABLE `note`
-  ADD CONSTRAINT `FK_Etudiant` FOREIGN KEY (`id_user`) REFERENCES `etudiant` (`id_user`),
-  ADD CONSTRAINT `FK_Module` FOREIGN KEY (`id_module`) REFERENCES `module` (`id_module`);
+  ADD CONSTRAINT `FK_Etudiant2` FOREIGN KEY (`fk_etudiant`) REFERENCES `etudiant` (`id_etudiant`),
+  ADD CONSTRAINT `FK_Module1` FOREIGN KEY (`fk_module`) REFERENCES `module` (`id_module`);
 
 --
 -- Constraints for table `seance`
 --
 ALTER TABLE `seance`
-  ADD CONSTRAINT `FK_Enseigne` FOREIGN KEY (`id_user`) REFERENCES `enseignant` (`id_user`),
-  ADD CONSTRAINT `FK_Occupe` FOREIGN KEY (`id_module`) REFERENCES `module` (`id_module`);
+  ADD CONSTRAINT `FK__seance_enseignant` FOREIGN KEY (`fk_seance_enseignant`) REFERENCES `enseignant` (`id_enseignant`),
+  ADD CONSTRAINT `FK__seance_module` FOREIGN KEY (`fk_seance_module`) REFERENCES `module` (`id_module`),
+  ADD CONSTRAINT `FK_module` FOREIGN KEY (`fk_seance_module`) REFERENCES `module` (`id_module`),
+  ADD CONSTRAINT `FK_user` FOREIGN KEY (`fk_seance_enseignant`) REFERENCES `enseignant` (`id_enseignant`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
